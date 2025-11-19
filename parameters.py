3d29208a6013a265890b8790e94af2c764990bc3
Niamh <niamhkathleenmclaughlin@hotmail.co.uk>
2025-11-19 18:31:04 +0000

    Updates

--- parameters.py
+++ parameters.py
@@ -1,4 +1,10 @@
-data_path="C:\\Users\\niamh\\OneDrive\\Desktop\\Analysis\\Raw_data\\SCN2A_EEG\\SCN2A\\month_6\\SCN2A_381\\TAINI_1047_B_SCN2A_381_SOM1_HELMET-2023_10_16-0000.dat"
+#taini data and metadata
+taini_data_path="C:\\Users\\niamh\\OneDrive\\Desktop\\Analysis\\Raw_data\\SCN2A_EEG\\SCN2A\\month_6\\SCN2A_381\\TAINI_1047_B_SCN2A_381_SOM1_HELMET-2023_10_16-0000.dat"
 
+number_of_channels = 16
+sample_rate = 250.4
+sample_datatype = 'int16'
+display_decimation = 1
+
+#annotations/labels file
 label_file_path = "C:\\MNE_Alfredo_update\\381_annot.csv"
-
