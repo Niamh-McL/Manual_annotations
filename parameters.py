8e2386b6c2a2d64903f297e783cf5158f43c1c06
Niamh <niamhkathleenmclaughlin@hotmail.co.uk>
2025-11-19 15:17:30 +0000

    Created parameters.py file and added initial paths

---
+++ parameters.py
@@ -0,0 +1,4 @@
+data_path="C:\\Users\\niamh\\OneDrive\\Desktop\\Analysis\\Raw_data\\SCN2A_EEG\\SCN2A\\month_6\\SCN2A_381\\TAINI_1047_B_SCN2A_381_SOM1_HELMET-2023_10_16-0000.dat"
+
+label_file_path = "C:\\MNE_Alfredo_update\\381_annot.csv"
+
