c8bbca5d8e77988b4deb1f2196c4f7f508f645d2
Niamh <niamhkathleenmclaughlin@hotmail.co.uk>
2025-11-19 18:48:15 +0000

    Updates

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import mne
+
+from mne.viz import set_browser_backend
 from processes import load_dat, load_labels
 from parameters import taini_data_path, label_file_path
 
@@ -11,5 +13,6 @@ custom_raw = load_dat(taini_data_path)
 annotations = load_labels(label_file_path)
 
 #Create interactive plot
+set_browser_backend('qt')
 annot_custom_raw = custom_raw.set_annotations(annotations)
 annot_custom_raw.plot(scalings = "auto", order=[0,1,2,3,4,6,7,8,9,10,11,12,13,14, 15], show_options = "true", block=True)
