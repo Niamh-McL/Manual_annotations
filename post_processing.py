9434692e5b29caf301abc15196358d2b6500fd06
Niamh <167229096+Niamh-McL@users.noreply.github.com>
2025-12-03 11:35:33 +0000

    Update post_processing.py
    Examples of what post-processing should be applied to annotations - need to code up.

--- post_processing.py
+++ post_processing.py
@@ -1,1 +1,15 @@
+# Filter manually annotated segments with criteria
 
+# minimum event duration (1s)  (define function in processes.py and call in post-processing.py)
+for event in labelled_events:
+  if duration < 1:
+    delete(event)
+  else:
+    continue
+
+# inter-event interval (define function in processes.py and call in post-processing.py)
+for event in labelled_events:
+  if event[n, onset+duration] - event[n-1, onset] < 1:
+    combine events
+  else:
+    continue
